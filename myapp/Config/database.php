484c790c6a2c2cc4a42205469b6b0a125f741c2d
salahuddin_mars <salahuddin@mars.com.bd>
2018-09-12 16:37:39 +0600

    first commit

---
+++ myapp/Config/database.php
@@ -0,0 +1,12 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => 'secret',
+		'database' => 'cake_blog_tutorial',
+	);
+}
